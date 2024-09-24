e0b9de7a58ec4e9cda47e44109ba57aed183de17
William Pittman <wspittman@gmail.com>
2024-09-24 10:13:36 -0700

    backend: add handler for 404 paths

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express from "express";
+import express, { Request, Response } from "express";
 import { config } from "./config";
 import { connectDB } from "./db/db";
 import { router } from "./routes";
@@ -10,6 +10,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", router);
+
+// For nonexistent routes
+app.use((_: Request, res: Response) => {
+  res.status(404).send("Not Found");
+});
 
 connectDB().then(() => {
   app.listen(config.PORT, () => {
