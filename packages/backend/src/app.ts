85dfbc691f91fca3694313781641116eff920e91
William Pittman <wspittman@gmail.com>
2024-09-24 10:53:25 -0700

    backend: global error handler

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,19 +1,32 @@
 import cors from "cors";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { config } from "./config";
 import { connectDB } from "./db/db";
 import { router } from "./routes";
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/api", router);
 
 // For nonexistent routes
 app.use((_: Request, res: Response) => {
   res.status(404).send("Not Found");
+});
+
+// Global error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    status: "error",
+    statusCode,
+    message,
+  });
 });
 
 connectDB().then(() => {
