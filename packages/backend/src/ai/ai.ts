78b8364077f02a5c3b76e20645d564ee84a5a191
William Pittman <wspittman@gmail.com>
2024-10-08 14:43:57 -0700

    backend: switch from json_object to json_schema return types

--- packages/backend/src/ai/ai.ts
+++ packages/backend/src/ai/ai.ts
@@ -3,18 +3,37 @@ import OpenAI from "openai";
 const client = new OpenAI();
 
 const locationPrompt = `You are an experienced job seeker whose goal is to quickly find relevant information from job descriptions.
-First, read the job location text that is provided. Then decide if the job is intended to be remote or on-site. Then decide where the job is based to the extent possible, regardless of whether it is remote or on-site. Provide the response in the following JSON format, using empty string ("") for any unknown fields.
-<format>
-{
-remote: boolean,
-city: string,
-state: string,
-country: string,
-}
-</format>
-In this context, "remote" should only be true for full remote jobs. If the job is hybrid or on-site, it should be false.
-In this context, "state" is shorthand for state, province, or similar.
-When well-known acronyms exist, always use the full name with the acronym in parentheses. For example, "Washington (WA)" or "United States of America (USA)".`;
+First, read the job location text that is provided. Then decide if the job is intended to be remote or hybrid/on-site. Then decide where the job is based to the extent possible, regardless of whether it is remote or hybrid/on-site.
+Provide the response JSON, using empty string ("") for any unknown fields.`;
+
+const locationSchema = {
+  name: "location_schema",
+  schema: {
+    type: "object",
+    properties: {
+      remote: {
+        type: "boolean",
+        description: "true for full remote jobs",
+      },
+      city: { type: "string", description: "city name" },
+      state: {
+        type: "string",
+        description: "state, province, or similar name",
+      },
+      stateAcronym: {
+        type: "string",
+        description: "Well-known acronym for the `state` field, if one exists",
+      },
+      country: { type: "string", description: "country name" },
+      countryAcronym: {
+        type: "string",
+        description:
+          "Well-known acronym for the `country` field, if one exists",
+      },
+    },
+    additionalProperties: false,
+  },
+};
 
 export async function extractLocation(locationText: string) {
   const f = await client.chat.completions.create({
@@ -29,8 +48,10 @@ export async function extractLocation(locationText: string) {
         content: `<location>${locationText}</location>`,
       },
     ],
-    // TBD: Switch to json_schema once it is supported with Azure gpt-4o-mini
-    response_format: { type: "json_object" },
+    response_format: {
+      type: "json_schema",
+      json_schema: locationSchema,
+    },
   });
   console.log(`tokens: ${f.usage?.total_tokens}`);
   console.log(f.choices[0].message);
