994d9d4f60a53c37e36363b6596f8a453716bcc6
William Pittman <wspittman@gmail.com>
2024-11-12 15:41:24 -0800

    frontend: Remove breadcrumbs from header. Fun, but not actually useful.

--- packages/frontend/src/frame/Header.tsx
+++ packages/frontend/src/frame/Header.tsx
@@ -1,10 +1,8 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link, { LinkProps } from "@mui/material/Link";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderLinkProps {
   url: string;
@@ -32,35 +30,10 @@ const HeaderLink = ({ url, text, variant }: HeaderLinkProps) => {
 };
 
 export const Header = (): JSX.Element => {
-  const location = useLocation();
-
-  const pathSegments = location.pathname.split("/").filter(Boolean);
-
-  const breadcrumbs = [
-    <HeaderLink key="home" url="/" text="Better Job Board" variant="h4" />,
-    ...pathSegments.map((segment, index) => {
-      const url = `/${pathSegments.slice(0, index + 1).join("/")}`;
-      const isLast = index === pathSegments.length - 1;
-
-      return isLast ? (
-        <Typography variant="h5" color="white" key={url}>
-          {segment.charAt(0).toUpperCase() + segment.slice(1)}
-        </Typography>
-      ) : (
-        <HeaderLink
-          key={url}
-          url={url}
-          text={segment.charAt(0).toUpperCase() + segment.slice(1)}
-          variant="h5"
-        />
-      );
-    }),
-  ];
-
   return (
     <AppBar position="sticky">
       <Toolbar variant="dense">
-        <Breadcrumbs aria-label="breadcrumb">{breadcrumbs}</Breadcrumbs>
+        <HeaderLink url="/" text="Better Job Board" variant="h4" />
         <Box sx={{ flexGrow: 1 }} />
         <HeaderLink url="/about" text="About" variant="h5" />
         <HeaderLink url="/explore" text="Explore" variant="h5" />
