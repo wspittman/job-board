bea2d0d7445921a27b1fdf32fece8b8f3c78348c
William Pittman <wspittman@gmail.com>
2025-10-09 17:02:51 -0700

    evals: update gemini models/pricing

--- packages/evals/src/evalConfig.ts
+++ packages/evals/src/evalConfig.ts
@@ -17,35 +17,36 @@ import type {
 } from "./portal/pTypes.ts";
 import type { Bag } from "./types/types.ts";
 
-// Model costs per million tokens [input, output], last pulled 9/25/2025
+// Model costs per million tokens [input, output], last pulled 10/9/2025
 export const llmModelCost: Record<string, [number, number]> = {
+  // No Reasoning Effort allowed
   "gpt-4o-mini": [0.15, 0.6],
   "gpt-4o": [2.5, 10.0],
 
+  // No Reasoning Effort allowed
   "gpt-4.1-nano": [0.1, 0.4],
   "gpt-4.1-mini": [0.4, 1.6],
   "gpt-4.1": [2.0, 8.0],
 
+  // Reasoning Effort allowed
   "gpt-5-nano": [0.05, 0.4],
   "gpt-5-mini": [0.25, 2.0],
   "gpt-5": [1.25, 10.0],
 
+  // Reasoning Effort required
   "o3-mini": [1.1, 4.4],
   "o4-mini": [1.1, 4.4],
   o3: [2.0, 8.0],
-};
 
-// OLD: Model costs per million tokens (input, output), last pulled 5/20/2025
-/*const google: ModelTuple[] = [
-  ["gemini-1.5-flash-8b", 0.0375, 0.15],
-  ["gemini-2.0-flash-lite", 0.075, 0.3],
-  ["gemini-2.0-flash", 0.1, 0.4],
-  ["gemini-2.5-flash", 0.15, 0.6],
-  ["gemini-2.5-flash-thinking", 0.15, 3.5],
-  ["gemini-1.5-pro", 1.25, 5.0],
-  ["gemini-1.5-pro-128k", 2.5, 10.0],
-  ["gemini-2.5-pro-preview-03-25", 1.25, 10.0],
-];*/
+  // Reasoning Effort
+  "gemini-2.5-pro": [1.25, 10.0],
+  "gemini-2.5-flash": [0.3, 2.5],
+  "gemini-2.5-flash-lite": [0.1, 0.4],
+
+  // No Reasoning Effort
+  "gemini-2.0-flash": [0.1, 0.4],
+  "gemini-2.0-flash-lite": [0.075, 0.3],
+};
 
 // #region Rubrics
 
