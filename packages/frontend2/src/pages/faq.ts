f70e1bc1e70f9ccdec7314018c64451efc1ef7f5
William Pittman <wspittman@gmail.com>
2025-10-17 10:26:20 -0700

    Fix FAQ refresh note display for all entries

--- packages/frontend2/src/pages/faq.ts
+++ packages/frontend2/src/pages/faq.ts
@@ -1,5 +1,5 @@
 import { api } from "../api/api";
-import { setDisplay, setText } from "../components/utils";
+import { setAllDisplay, setAllText } from "../components/utils";
 import "../sharedStyles/all.css";
 import "./faq.css";
 
@@ -8,8 +8,8 @@ try {
   const lastRefreshed = `Last refreshed: ${new Date(
     data.timestamp
   ).toLocaleString()}`;
-  setText(document, ".faq-note", lastRefreshed);
-  setDisplay(document, ".faq-note", "block");
+  setAllText(document, ".faq-note", lastRefreshed);
+  setAllDisplay(document, ".faq-note", "block");
 } catch (err) {
   // ignore
 }
