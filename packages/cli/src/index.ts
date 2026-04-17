a7a47fcb815ce496e1eb6bf456bf4e579785f552
William Pittman <wspittman@gmail.com>
2026-04-16 18:56:49 -0700

    cli: portal folder copy/paste

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,13 +1,39 @@
 import { logger } from "dry-utils-logger";
 import process from "node:process";
+import { atsTypes, dataModelTypes } from "./portal/pTypes.ts";
+import { CommandError } from "./types.ts";
 
-function main(): Promise<void> {
+function usageReminder() {
+  logger.info(
+    [
+      "Usage:",
+      "  npm run cli -- <COMMAND> <ARGS>",
+      "",
+      "  COMMAND:",
+      //...getUsage("    "),
+      "",
+      "  COMMON ARGUMENTS:",
+      `    DATA_MODEL: ${dataModelTypes.join("|")}`,
+      `    ATS: ${atsTypes.join("|")}`,
+      "",
+    ].join("\n"),
+  );
+}
+
+function main() {
   const [command, ...args] = process.argv.slice(2);
   logger.info(`Running command "${command}"`, args);
-  return Promise.resolve();
+  //await runCommand(command, args);
+  if (command === "testOK") {
+    return Promise.resolve();
+  }
+  throw new CommandError("No commands implemented yet");
 }
 
 main().catch((err) => {
+  if (err instanceof CommandError) {
+    usageReminder();
+  }
   logger.error("Main:", err);
   process.exitCode = 1;
 });
